9c4c2061b365f8c8c7611231af47b7ed0f94c868
Rafael <rafinhapsantos50@gmail.com>
2022-04-07 20:14:49 -0300

    primeiro commit

---
+++ assets/scripts/descricaoLinguagens.js
@@ -0,0 +1,71 @@
+// Mudar a Descrição
+
+const changeDescription = (nameCard, text) => {
+    let infoSkills = document.querySelector('#info-skills')
+    let cardName = document.querySelector('.card-name')
+
+    document.querySelector(nameCard).addEventListener('mouseover', () => {
+        infoSkills.innerHTML = text
+        if(nameCard === '.html') {
+            cardName.innerHTML = ' Html '
+        } 
+        if(nameCard === '.css') {
+            cardName.innerHTML = ' Css '
+        }
+        if(nameCard === '.js') {
+            cardName.innerHTML = ' JavaScript '
+        }
+        if(nameCard === '.git') {
+            cardName.innerHTML = ' Git '
+        } 
+        if(nameCard === '.java') {
+            cardName.innerHTML = ' Java '
+        } 
+        if(nameCard === '.mysql') {
+            cardName.innerHTML = ' MySQL '
+        } 
+        if(nameCard === '.angular') {
+            cardName.innerHTML = ' ANGULAR '
+        } 
+    })
+
+    document.querySelector(nameCard).addEventListener('mouseout', () => {
+        infoSkills.innerHTML = '* passe o cursor sobre os ícones para saber mais *'
+        cardName.innerHTML = ""
+    })
+}
+
+changeDescription(
+    '.html',
+    'Estrutura de layouts, principais tags, HTML Semântico, criação de tabelas e formulários.'
+)
+
+changeDescription(
+    '.css',
+    'Estilização de layouts, box model, CSS Grid, FlexBox.'
+)
+
+changeDescription(
+    '.js',
+    'Conceitos basicos da linguagem, tipos de dados, métodos, funções e eventos.'
+)
+
+changeDescription(
+    '.git',
+    'Versionamento de código e principais comandos.'
+)
+
+changeDescription(
+    '.java',
+    'Essa é minha linguagem principal, a que eu mais me aprofundei, tenho conhecimento em POO, JavaFX, JavaWEB.'   
+)
+
+changeDescription(
+    '.mysql',
+    'Criação e gerenciamente de um banco de dados, conhecimento dos principais comandos.'   
+)
+changeDescription(
+    '.angular',
+    'Estou me aprofundando nesse framework, estou gostando da estrutura do mesmo.'   
+)
+
